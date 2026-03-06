7170df0ee96c39e2df9cf2ce4b3e1b8231105234
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-03-06 20:58:05 +0600

    New

--- Assignment_1_1.cpp
+++ Assignment_1_1.cpp
@@ -2,7 +2,43 @@
 using namespace std;
 
 int main() {
-    cout<<5;
+    int n,i;
+    cin>>n;
+    int arr[n];
+
+    for(i=1; i<=n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout << "Before inserting a elemnt " << endl;
+
+      for(i=1; i<=n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    int  t = n;
+    int position,x;
+    cout << "Inserting an element in position : ";
+    cin>>position;
+    cout << "Insert : " ;
+    cin>>x;
+
+    while(t>=position)
+    {
+        arr[t+1] = arr[t];
+        t--;
+    }
+    arr[position] = x;
+
+    cout << "After Inserting : " << endl;
+     for(i=1; i<=n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
 
     return 0;
 }
