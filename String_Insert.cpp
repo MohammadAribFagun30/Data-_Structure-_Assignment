1658c1c351e8dd8ae5cd852cbd7cd5f52647e745
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-03-17 23:17:37 +0600

    DS Assignment

--- String_Insert.cpp
+++ String_Insert.cpp
@@ -1,8 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+
+    string s;
+    getline(cin, s);
+
+    cout << "Item to insert : ";
+    char item;
+    cin >> item;
+
+    int index;
+    cout << "Inserting in index : ";
+    cin >> index;
+
+    int len = s.length();
+    s.resize(len + 1);
+    while (len >= index)
+    {
+        s[len] = s[len - 1];
+
+        len--;
+    }
+
+    s[index] = item;
+
+    cout << "After Inserting in String : " << s;
 
     return 0;
 }
