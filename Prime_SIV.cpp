ae2f7ae1bec4cd5e2d593b74c637939d840519eb
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-04-22 21:18:38 +0600

    DS Assignment

---
+++ Prime_SIV.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool isPrime[n + 1];
+
+    // Initialize all as true
+    for (int i = 0; i <= n; i++) {
+        isPrime[i] = true;
+    }
+
+    isPrime[0] = false;
+    isPrime[1] = false;
+
+    // Sieve logic
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    if (isPrime[n])
+        cout << "Prime";
+    else
+        cout << "Not Prime";
+
+    return 0;
+}
